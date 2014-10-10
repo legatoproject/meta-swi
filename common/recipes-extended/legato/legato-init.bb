57fabda5b1ed818a60077abccd70abb1e7447991
Bertrand Roussel <broussel@sierrawireless.com>
2014-10-10 12:39:30 -0700

    Move legato-init to meta-swi
    Change-Id: Ieee6907384187a5ac68d1ef66c88e740f31b25f3

---
+++ common/recipes-extended/legato/legato-init.bb
@@ -0,0 +1,30 @@
+DESCRIPTION = "Sierra Wireless Legato Initialization"
+HOMEPAGE = "http://www.sierrawireless.com"
+LICENSE = "SierraWireless-Proprietary"
+LIC_FILES_CHKSUM = "file://../startlegato.sh;startline=2;endline=2;md5=3a6143426a9c75229175e743950f0ddc"
+
+SRC_URI = " \
+          file://startlegato.sh \
+          "
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+
+do_configure() {
+    :
+}
+
+do_compile() {
+    :
+}
+
+do_install_append () {
+    install -m 0755 ${WORKDIR}/startlegato.sh -D ${D}${sysconfdir}/init.d/startlegato.sh
+
+    [ -n "${D}" ] && OPT="-r ${D}" || OPT="-s"
+    update-rc.d $OPT -f startlegato.sh remove
+    update-rc.d $OPT startlegato.sh start 99 5 . stop 01 S 0 6 .
+}
+
+FILES_${PN} = " \
+          ${sysconfdir}/ \
+              "
---
+++ common/recipes-extended/legato/legato-init/startlegato.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+# Copyright 2013 Sierra Wireless
+#
+# Provides a hook for legato into the init scripts
+
+
+STARTUP_DIR=/mnt/flash/startup
+STOP_FILE1=".STOPLEGATO"
+STOP_FILE2="STOPLEGATO"
+
+case "$1" in
+    start)
+
+        echo "Legato start sequence"
+        # Make sure the directory actually exists
+        if [ ! -d $STARTUP_DIR ]
+        then
+          echo "Startup directory not found. Exiting Legato startup"
+          exit 0
+        fi
+
+        # Check our directory to see if there is anything to run
+        
+        files=`ls $STARTUP_DIR`
+        cd $STARTUP_DIR
+
+        if [ -f $STOP_FILE1 -o -f $STOP_FILE2 ]
+        then
+          echo "Legato startup sequence aborted"
+          exit 0
+        else
+          echo "stop file not present ... continuing"
+        fi
+
+        fgfiles=`echo $files | grep fg_`
+        bgfiles=`echo $files | grep -v fg_`
+
+        for file in $fgfiles
+        do
+          # Execute these files in the foreground
+          if [ -x $file -a -f $file -a -O $file ]
+          then
+            echo "Executing $file"
+            ./${file}
+          fi
+        done
+
+        # Now handle the background ones
+
+        for file in $bgfiles
+        do
+          # Execute these files in the foreground
+          if [ -x $file -a -f $file -a -O $file ]
+          then
+            echo "Executing $file in the background"
+            ./${file} &
+          fi
+        done
+        ;;
+
+    stop)
+        # Do something to stop Legato
+        echo "Legato shutdown sequence"
+        if [ -f /usr/local/bin/legato ]
+        then 
+            /usr/local/bin/legato stop
+        fi
+        ;;
+
+    *)
+        exit 1
+        ;;
+
+esac
+
+echo Finished Legato Sequence
+
