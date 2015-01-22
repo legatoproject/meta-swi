17d47febd1528b412b885ccb77ea86f5f356ca25
Mihai Dragusu <mihai.dragusu@windriver.com>
2015-01-22 16:45:12 +0200

    update tty dev test
    Signed-off-by: Mihai Dragusu <mihai.dragusu@windriver.com>

--- common/recipes-devtools/userspace-test/files/userspace-test-0.1/tty_dev_test.c
+++ common/recipes-devtools/userspace-test/files/userspace-test-0.1/tty_dev_test.c
@@ -1,4 +1,7 @@
 #include <termios.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <getopt.h>
@@ -6,83 +9,67 @@
 int
 main(int argc, char *argv[])
 {
-  int fd, serial, status;
+  
+  int fd, serial, status, lstatus;
+  serial = 0; 
+  status = 0;
+  lstatus = 0;
   static const char *device = "/dev/ttyHSL1";
-  //device = optarg;
 
-  fd = open(device, O_RDONLY);
-  ioctl(fd, TIOCMGET, &status);
-  printf("status is %d \n",status);
+ fd = open(device, O_RDWR);
+  //All in
+  status |= TIOCM_LE|TIOCM_DTR|TIOCM_RTS|TIOCM_ST|TIOCM_SR|TIOCM_CTS|TIOCM_CAR|TIOCM_RNG|TIOCM_DSR;
+  ioctl(fd, TCSETSF, &status);
+
+  usleep(200);
+  
+  ioctl(fd, TIOCMGET, &serial);
+  printf ("serial is %x \n",serial);
+  
   close(fd);
+  
+fd = open(device, O_RDWR);
 
+    status &= ~TIOCM_DTR;
+  ioctl(fd, TIOCMSET, &status);
+  usleep(200);
+  ioctl(fd, TIOCMGET, &serial);
+  printf ("serial is %x \n",serial);
+  
 
-  fd = open(device, O_RDWR);
-    
- if (ioctl(fd, TIOCMSET, TIOCM_LE & status) < 0) {
-    printf("Failed to set TIOCM_LE \n");
-    
- }
+  status &= ~TIOCM_RTS;
+  ioctl(fd, TIOCMSET, &status);
+  usleep(200);
+  ioctl(fd, TIOCMGET, &serial);
+  printf ("serial is %x \n",serial);
 
-  if (ioctl(fd, TIOCMSET, TIOCM_RI) < 0) {
-    printf("Failed to set TIOCM_RI \n");
- }
+  
+  status &= ~TIOCM_ST;
+  ioctl(fd, TIOCMSET, &status);
+  usleep(200);
+  ioctl(fd, TIOCMGET, &serial);
+  printf ("serial is %x \n",serial);
 
+  status &= ~TIOCM_SR;
+  ioctl(fd, TIOCMSET, &status);
+  usleep(200);
+  ioctl(fd, TIOCMGET, &serial);
+  printf ("serial is %x \n",serial);
 
-  if (ioctl(fd, TIOCMSET, TIOCM_CAR) < 0) {
-    printf("Failed to set TIOCM_CAR \n");
- }
+  ioctl(fd, TIOCGSOFTCAR, &lstatus);
+  printf ("clocal status is  %x \n",lstatus);
 
-  if (ioctl(fd, TIOCMSET, TIOCM_CTS) < 0) {
-    printf("Failed to set TIOCM_CTS \n");
- }
+  ioctl(fd, TIOCSSOFTCAR, TIOCM_RTS);
+  ioctl(fd, TIOCGSOFTCAR, &lstatus);
+  printf ("clocal status is  %x \n",lstatus);
+
+  status &= ~TIOCM_RTS;
+  ioctl(fd, TIOCMSET, &status);
+  usleep(200);
+  ioctl(fd, TIOCMGET, &serial);
+  printf ("serial is %x \n",serial);
+  
+  //printf ("clocal status is  %x \n",lstatus);
 
   close(fd);
-
-    //msm_hs_get_mctrl_locked (device);
-    
-    fd = open(device, O_RDONLY);
-
-    ioctl(fd, TIOCMGET, &serial);
-    if (serial & TIOCM_DTR)
-        puts("TIOCM_DTR is not set");
-    else
-        puts("TIOCM_DTR is set");
-
-    if (serial & TIOCM_LE)
-        puts("TIOCM_LE is not set");
-    else
-        puts("TIOCM_LE is set");
-    
-    if (serial & TIOCM_CTS)
-        puts("TIOCM_CST is not set");
-    else
-        puts("TIOCM_CST is set");
-    
-    if (serial & TIOCM_CAR)
-        puts("TIOCM_CAR is not set");
-    else
-        puts("TIOCM_CAR is set");
-    
-    if (serial & TIOCM_RTS)
-        puts("TIOCM_RTS is not set");
-    else
-        puts("TIOCM_RTS is set");
-    
-    if (serial & TIOCM_ST)
-        puts("TIOCM_ST is not set");
-    else
-        puts("TIOCM_ST is set");
-
-    if (serial & TIOCM_SR)
-        puts("TIOCM_SR is not set");
-    else
-        puts("TIOCM_SR is set");
-
-    if (serial & TIOCM_RI)
-        puts("TIOCM_RI is not set");
-    else
-        puts("TIOCM_RI is set");
-
-
-    close(fd);
 }
