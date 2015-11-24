33cc4ba41a5c489495c8cd7098c21c635ce91bc7
Stephen Coulson <scoulson@sierrawireless.com>
2015-11-24 10:32:29 -0800

    ticket:3362 Minimize startlegato.sh in Yocto build rcS.d
     - modify startlegato.sh
    
    Change-Id: I2027d92cace79f2b134a9a057d40b942be2e088c

--- common/recipes-legato/legato-init/legato-init.bb
+++ common/recipes-legato/legato-init/legato-init.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = "Sierra Wireless Legato Initialization"
 HOMEPAGE = "http://www.sierrawireless.com"
 LICENSE = "SierraWireless-Proprietary"
-LIC_FILES_CHKSUM = "file://../startlegato.sh;startline=2;endline=2;md5=3a6143426a9c75229175e743950f0ddc"
+LIC_FILES_CHKSUM = "file://../startlegato.sh;startline=2;endline=2;md5=0357211a003ec058e91c7f12c9922033"
 
 SRC_URI = " \
           file://startlegato.sh \
--- common/recipes-legato/legato-init/legato-init/startlegato.sh
+++ common/recipes-legato/legato-init/legato-init/startlegato.sh
@@ -1,77 +1,89 @@
 #!/bin/sh
-# Copyright 2013 Sierra Wireless
+# Copyright (c) Sierra Wireless, Inc. Use of this work is subject to license.
 #
 # Provides a hook for legato into the init scripts
+#
+# The new legato start procedure begins at /mnt/legato/start
+# If not found an attempt will be made to start an old style legato in case
+# a user has accidentally installed new firmware in cwe without updating the built in legato.
+# If this works it provides a chance for the user to correct the problem and upload a new cwe.
+# No attempt will be made to install any old style Legato that may be found in the built in
+# legato partition.
 
 beginswith() { case $2 in $1*) true;; *) false;; esac; }
 
+LEGATO_START=/mnt/legato/start
+
+# This is where old startup lived. We can try this if we we don't have new startup.
 STARTUP_DIR=/mnt/flash/startup
-STOP_FILE1=".STOPLEGATO"
-STOP_FILE2="STOPLEGATO"
-PRE_INSTALL_DIR=/mnt/legato/usr/local/bin
 
 case "$1" in
     start)
 
-        # Check for a new uninitialized install
-        if [ -x $PRE_INSTALL_DIR/legato ]
+        if [ -x $LEGATO_START ]
         then
-            $PRE_INSTALL_DIR/legato bootcheck
-        fi
-
-        echo "Legato start sequence"
-        # Make sure the directory actually exists
-        if [ ! -d $STARTUP_DIR ]
-        then
-          echo "Startup directory not found. Exiting Legato startup"
-          exit 0
-        fi
-
-        # Check our directory to see if there is anything to run
-        files=`ls $STARTUP_DIR`
-        cd $STARTUP_DIR
-
-        if [ -f $STOP_FILE1 -o -f $STOP_FILE2 ]
-        then
-          echo "Legato startup sequence aborted"
-          exit 0
+            $LEGATO_START &
         else
-          echo "stop file not present ... continuing"
-        fi
 
-        # List the files in the order of their numeric id
-        # Assumes fg_xx_script, or bg_xx_script where
-        # xx identifies the start order
+            # Check for an old style startup and see if we can make it run.
+            # This code will be removed in a not too distant future rootfs release.
 
-        files=`ls -1 | sort -k2 -t"_"`
-
-        for file in $files
-        do
-          # Execute the files in order
-          if [ -x $file -a -f $file -a -O $file ]
-          then
-            if beginswith fg_ "$file"; then
-              echo "Executing $file in foreground"
-              ./${file}
-            elif beginswith bg_ "$file"; then
-              echo "Executing $file in background"  
-              ./${file}&
+            echo "WARNING: Valid Legato install not found."
+            echo "Checking for legacy version and attempting alternate start up."
+            # Make sure the directory actually exists
+            if [ ! -d $STARTUP_DIR ]
+            then
+              echo "ERROR: Legato install missing, out of date or corrupted."
+              echo "Exiting Legato startup."
+              exit 0
             fi
-          fi
-        done
 
-        if [ -x $PRE_INSTALL_DIR/legato ]
-        then
-            $PRE_INSTALL_DIR/legato postbootcheck
+            # Check our directory to see if there is anything to run
+            files=`ls $STARTUP_DIR`
+            cd $STARTUP_DIR
+
+            # List the files in the order of their numeric id
+            # Assumes fg_xx_script, or bg_xx_script where
+            # xx identifies the start order
+
+            files=`ls -1 | sort -k2 -t"_"`
+
+            for file in $files
+            do
+              # Execute the files in order
+              if [ -x $file -a -f $file -a -O $file ]
+              then
+                if beginswith fg_ "$file"; then
+                  echo "Executing $file in foreground"
+                  ./${file}
+                elif beginswith bg_ "$file"; then
+                  echo "Executing $file in background"
+                  ./${file}&
+                fi
+              fi
+            done
+
         fi
         ;;
 
     stop)
         # Do something to stop Legato
         echo "Legato shutdown sequence"
-        if [ -f /usr/local/bin/legato ]
-        then 
-            /usr/local/bin/legato stop
+        if [ -x $LEGATO_START ]
+        then
+            $LEGATO_START stop
+        else
+            # We may have an old style legato install. Try to stop it.
+            # This may not work but might as well try.
+            # This code will be removed in a not too distant future rootfs release.
+            echo "WARNING: Valid Legato install not found."
+            echo "Checking for legacy version and attempting alternate shut down"
+            if [ -f /usr/local/bin/legato ]
+            then
+                /usr/local/bin/legato stop
+            else
+                echo "ERROR: Legato install missing, out of date or corrupted."
+            fi
         fi
         ;;
 
