9ffcdb3d92a312441f37334b96541261f846d024
Zoran Markovic <zmarkovic@sierrawireless.com>
2018-01-19 19:55:06 -0800

    build: enable build of open-source layers only
    Move build script and makefiles to meta-swi so that they could be
    used if meta-swi layer is built without meta-swi-extras. When
    meta-swi-extras layer is absent, assume that an open-source-only
    build is invoked.
    
    Layers meta-mangoh and legato have meta-swi-extras dependencies
    to sierra-m2m-init and qmi-framework, respectively. Also, target
    machine is determined based on the presence of binary layer files
    in meta-swi-extras. In absence of these files, the build has to
    be told for which target machine it is being created. This means
    that the command for open-source build would have to be:
    
        MANGOH_BUILD=0 LEGATO_BUILD=0 MACH=<mach> make image_bin
    
    where <mach> is one of mdm9x15, mdm9x28, etc.
    
    Resolves: LXSWIREF-253
    Change-Id: I59a2ed0be6c1b667a1d2e730b5b9fca5056b08ac
    Signed-off-by: Zoran Markovic <zmarkovic@sierrawireless.com>

---
+++ build.sh
@@ -0,0 +1,572 @@
+#!/bin/bash
+#
+# Copyright (c) 2011-2012 Wind River Systems, Inc.
+#
+
+# Let bitbake use the following env-vars as if they were pre-set bitbake ones.
+# (BBLAYERS is explicitly blocked from this within OE-Core itself, though...)
+export BB_ENV_EXTRAWHITE="http_proxy MACHINE DISTRO DL_DIR"
+
+IFS='
+    '
+
+PATH=/usr/local/bin:/bin:/usr/bin
+export PATH
+
+
+UMASK=022
+umask $UMASK
+
+scriptdir=$(cd $(dirname ${0}); pwd)
+WS="$scriptdir/../poky"
+
+ALL_ARGS="$*"
+
+usage()
+{
+    cat << EOF
+Usage:
+$0 <options ...>
+
+  Global:
+    -p <poky_dir>
+    -o <meta-openembedded dir>
+    -l <SWI meta layer>
+    -x <linux repo directory>
+    -m <SWI machine type>
+    -b <build_dir>
+    -t <number of bitbake tasks>
+    -j <number of make threads>
+    -r (enable preempt-rt kernel <Test-only. Not supported>)
+    -g (enable Legato setup and build Legato images)
+    -a (pass extra options for recipes, key=value separated by ::)
+    -K <kernel provider>
+    -e (enable build recovery image or normal image)
+
+  Machine swi-mdmXXXX:
+    -q (enable Qualcomm Proprietary bin)
+    -s (enable Qualcomm Proprietary source)
+    -w <Qualcomm source directory (apps_proc)>
+    -v <version of Qualcomm sources>
+    -F <path to ar_yocto-cwe.tar.bz2>
+    -M (enable mangoh meta layer)
+    -Q (building for Qemu)
+
+  Task control:
+    -c (enable command line mode)
+    -d (build the full debug image)
+    -k (build the toolchain)
+EOF
+}
+
+usage_and_exit()
+{
+    usage
+    exit $1
+}
+
+if [ $# = 0 ]; then
+    usage_and_exit 1
+fi
+
+# Default options
+BD="$scriptdir/../build"
+MACH=swi-mdm9x28
+DEBUG=false
+TASKS=4
+THREADS=4
+CMD_LINE=false
+TOOLCHAIN=false
+ENABLE_PROPRIETARY=false
+ENABLE_PROPRIETARY_SRC=false
+ENABLE_ICECC=false
+ENABLE_LEGATO=false
+ENABLE_META_MANGOH=false
+DISTRO=poky-swi
+KERNEL_PROVIDER=
+X_OPTS=
+PROD=
+ENABLE_RECOVERY=
+QEMU=false
+
+while getopts ":p:o:b:l:x:m:t:j:w:v:a:F:P:MecdrqsgkhQ" arg
+do
+    case $arg in
+    p)
+        WS=$(readlink -f $OPTARG)
+        echo "Poky dir: $WS"
+        ;;
+    o)
+        OE=$(readlink -f $OPTARG)
+        echo "OE meta: $OE"
+        ;;
+    b)
+        BD=$(readlink -f $OPTARG)
+        echo "Build dir: $BD"
+        ;;
+    l)
+        SWI=$(readlink -f $OPTARG)
+        echo "SWI meta dir: $SWI"
+        ;;
+    x)
+        LINUXDIR=$(readlink -f $OPTARG)
+        echo "Linux repo dir: $LINUXDIR"
+        ;;
+    m)
+        MACH=$OPTARG
+        echo "SWI machine: $MACH"
+        ;;
+    d)
+        DEBUG=true
+        echo "Enable more packages for debugging"
+        ;;
+    t)
+        TASKS=$OPTARG
+        echo "Number of bitbake tasks $TASKS"
+        ;;
+    j)
+        THREADS=$OPTARG
+        echo "Number of make threads $THREADS"
+        ;;
+    a)
+        X_OPTS="$X_OPTS $OPTARG"
+        echo "Extra options added -  $OPTARG"
+        ;;
+    K)
+        KERNEL_PROVIDER="$OPTARG"
+        echo "Kernel provider: $KERNEL_PROVIDER"
+        ;;
+    e)
+        ENABLE_RECOVERY="true"
+        echo "Enable recovery image: $ENABLE_RECOVERY"
+        ;;
+    c)  CMD_LINE=true
+        echo "Enable command line mode"
+        ;;
+    q)  ENABLE_PROPRIETARY=true
+        echo "Enable Qualcomm Proprietary bin"
+        ;;
+    s)  ENABLE_PROPRIETARY_SRC=true
+        echo "Enable Qualcomm Proprietary source - overrides binary option"
+        ;;
+    w)
+        WK=$(readlink -f $OPTARG)
+        [ -n "$WK" ] || echo "warning: -w $OPTARG (for WK variable) doesn't resolve"
+        ;;
+    v)
+        FW_VERSION=$OPTARG
+        echo "FW Version: $FW_VERSION"
+        ;;
+    g)
+        ENABLE_LEGATO="true"
+        echo "With Legato"
+        ;;
+    k)  TOOLCHAIN=true
+        echo "Build toolchain"
+        ;;
+    h)  ENABLE_ICECC=true
+        echo "Build using icecc"
+        ;;
+    F)  FIRMWARE_PATH=$(readlink -f $OPTARG)
+        echo "Use FIRWARE_PATH=${FIRMWARE_PATH} to fetch ar_yocto-cwe.tar.bz2 binary"
+        ;;
+    M)  ENABLE_META_MANGOH=true
+        echo "With mangOH"
+        ;;
+    P)
+        PROD=$OPTARG
+        echo "SWI product: $PROD"
+        ;;
+    Q)  QEMU=true
+        echo "Building for QEMU"
+        ;;
+    ?)
+        echo "$0: invalid option -$OPTARG" 1>&2
+        usage_and_exit 1
+        ;;
+    esac
+done
+
+. ${WS}/oe-init-build-env $BD
+
+## Check: bash
+
+# Make sure that bash is set as default or build is not guaranteed to work
+if [[ "$(basename $(readlink -f /bin/sh))" != "bash" ]]; then
+    echo "Error: bash is not set as default provider for /bin/sh"
+    echo "       build is not guaranteed to work, aborting"
+    exit 1
+fi
+
+## Conf: bblayers.conf
+declare -a LAYERS
+
+enable_layer()
+{
+    local LAYER_NAME=$1
+    local LAYER_PATH=$2
+    local PREVIOUS_LAYER=$3
+
+    if [ -z "$PREVIOUS_LAYER" ]; then
+        PREVIOUS_LAYER='meta-yocto-bsp'
+    fi
+
+    echo "+ layer: $LAYER_NAME"
+
+    LAYERS+=("$LAYER_PATH")
+
+    grep -E "/$LAYER_NAME " $BD/conf/bblayers.conf > /dev/null
+    if [ $? != 0 ]; then
+        echo "         -> $LAYER_PATH"
+        sed -e '/'"$PREVIOUS_LAYER"'/a\  '"$LAYER_PATH"' \\' -i $BD/conf/bblayers.conf
+    fi
+}
+
+case $MACH in
+    swi-virt-* )
+        enable_layer "meta-swi-virt" "$SWI/meta-swi-virt"
+        ;;
+    swi-mdm9* )
+        # Enable the common meta-swi-mdm9xxx layer
+        enable_layer "meta-swi/meta-swi-mdm9xxx" "$SWI/meta-swi-mdm9xxx"
+
+        # Enable the meta-swi-mdmNNNN layer
+        enable_layer "meta-swi/meta-$MACH" "$SWI/meta-$MACH" "meta-swi-mdm9xxx"
+
+        # Enable the meta-swi-mdmNNNN-PROD layer, if it exists
+        if [ -n "$PROD" ] && [ -e "$SWI/meta-${MACH}-${PROD}" ]; then
+            enable_layer "meta-swi/meta-$MACH-$PROD" "$SWI/meta-$MACH-$PROD" "meta-$MACH"
+        fi
+
+        if [ $ENABLE_PROPRIETARY_SRC = true ] || [ $ENABLE_PROPRIETARY = true ]; then
+            # Distro to poky-swi-ext to change SDKPATH
+            DISTRO="poky-swi-ext"
+        fi
+        ;;
+esac
+
+# Enable the meta-swi layer
+enable_layer "meta-swi/common" "$SWI/common"
+
+# Enable the meta-oe layer
+enable_layer "meta-oe" "$OE/meta-oe"
+
+# Enable the meta-networking layer
+enable_layer "meta-networking" "$OE/meta-networking"
+
+# Enable the meta-python layer
+enable_layer "meta-python" "$OE/meta-python"
+
+# Enable proprietary layers: common
+if [ $ENABLE_PROPRIETARY_SRC = true ] || [ $ENABLE_PROPRIETARY = true ]; then
+    enable_layer "meta-swi-extras/common" "$scriptdir/../meta-swi-extras/common" "meta-$MACH"
+
+    enable_layer "meta-swi-extras/meta-$MACH" "$scriptdir/../meta-swi-extras/meta-$MACH" "meta-$MACH"
+fi
+
+# Enable meta-mangoh layer
+if [ $ENABLE_META_MANGOH = true ]; then
+    enable_layer "meta-mangoh" "$scriptdir/../meta-mangoh"
+fi
+
+# Enable proprietary layers: from sources
+if [ $ENABLE_PROPRIETARY_SRC = true ]; then
+    # Check that we have a source workspace to point to
+    WK=${WK:-$WORKSPACE}
+    WORKSPACE=${WK:?"Not set - must point to apps_proc of firmware build"}
+
+    if [ ! -d ${WORKSPACE}/qmi ]; then
+        echo WORKSPACE \(${WORKSPACE}\) appears not to be a valid source location
+        echo WORKSPACE must point to apps_proc of firmware build
+        exit 1
+    fi
+
+    echo "Workspace dir: $WORKSPACE"
+    export WORKSPACE
+
+    if [ -n "$FW_VERSION" ]; then
+        echo "Workspace version: $FW_VERSION"
+        export FW_VERSION
+    fi
+
+    # Add common mdm9xxx source layer
+    enable_layer "meta-swi-extras/meta-swi-mdm9xxx-src" "$scriptdir/../meta-swi-extras/meta-swi-mdm9xxx-src" "meta-swi-extras\/common"
+
+    # Add machine-specific source layer
+    enable_layer "meta-swi-extras/meta-$MACH-src" "$scriptdir/../meta-swi-extras/meta-$MACH-src" "meta-swi-mdm9xxx-src"
+
+    # Add product-specific source layer
+    if [ -n "$PROD" ] && [ -e "$scriptdir/../meta-swi-extras/meta-$MACH-$PROD-src" ]; then
+        enable_layer "meta-swi-extras/meta-$MACH-$PROD-src" \
+            "$scriptdir/../meta-swi-extras/meta-$MACH-$PROD-src" \
+            "meta-$MACH-src"
+    fi
+
+    copy_qmi_api() {
+        cp -f $WORKSPACE/../modem_proc/sierra/src/dx/src/common/* $WORKSPACE/sierra/dx/common
+        if [ $? != 0 ]; then
+            echo "Unable to copy dx common files from modem_proc"
+            exit 1
+        fi
+        cp -f $WORKSPACE/../modem_proc/sierra/src/dx/api/common/* $WORKSPACE/sierra/dx/common
+        if [ $? != 0 ]; then
+            echo "Unable to copy dx common API files from modem_proc"
+            exit 1
+        fi
+        # We have to copy the qapi files from the modem dir
+        cp -f $WORKSPACE/../modem_proc/sierra/src/qapi/src/common/* $WORKSPACE/sierra/qapi/common
+        if [ $? != 0 ]; then
+            echo "Unable to copy qapi common files from modem_proc"
+            exit 1
+        fi
+        # Provice common qcsi files as a link to common qapi files
+        rm -rf $WORKSPACE/sierra/qcsi/common && ( cd $WORKSPACE/sierra/qcsi && ln -s ../qapi/common common )
+        if [ $? != 0 ]; then
+            echo "Unable to provide qcsi common files from modem_proc"
+            exit 1
+        fi
+        # We have to copy the NV files from the modem dir
+        cp -f $WORKSPACE/../modem_proc/sierra/src/nv/src/common/* $WORKSPACE/sierra/nv/common
+        if [ $? != 0 ]; then
+            echo "Unable to copy NV common files from modem_proc"
+            exit 1
+        fi
+        cp -f $WORKSPACE/../modem_proc/sierra/src/nv/api/common/* $WORKSPACE/sierra/nv/common
+        if [ $? != 0 ]; then
+            echo "Unable to copy NV API files from modem_proc"
+            exit 1
+        fi
+    }
+
+    if [[ "$MACH" == "swi-mdm9x15" ]]; then
+        # We have to copy the dx files from the modem dir
+        copy_qmi_api
+    fi
+fi
+
+# Enable proprietary layers: from binaries
+if [ $ENABLE_PROPRIETARY = true ]; then
+    # Add common mdm9xxx binary layer
+    enable_layer "meta-swi-extras/meta-swi-mdm9xxx-bin" "$scriptdir/../meta-swi-extras/meta-swi-mdm9xxx-bin" "meta-$MACH"
+
+    # Add machine-specific binary layer
+    enable_layer "meta-swi-extras/meta-$MACH-bin" "$scriptdir/../meta-swi-extras/meta-$MACH-bin" "meta-swi-mdm9xxx-bin"
+
+    # Add product-specific source layer
+    if [ -n "$PROD" ] && [ -e "$scriptdir/../meta-swi-extras/meta-$MACH-$PROD-bin" ]; then
+        enable_layer "meta-swi-extras/meta-$MACH-$PROD-bin" \
+            "$scriptdir/../meta-swi-extras/meta-$MACH-$PROD-bin" \
+            "meta-$MACH-bin"
+    fi
+fi
+
+## Conf: local.conf
+
+set_option() {
+    opt_name=$1
+    opt_val=$2
+
+    if grep "$opt_name =" $BD/conf/local.conf > /dev/null; then
+        # Update entry
+        echo "Updating $opt_name to $opt_val"
+        sed -e "s/$opt_name = \".*//g" -i $BD/conf/local.conf
+        echo "$opt_name = \"$opt_val\"" >> $BD/conf/local.conf
+    else
+        # Append
+        echo "Adding option $opt_name with value $opt_val"
+        echo "$opt_name = \"$opt_val\"" >> $BD/conf/local.conf
+    fi
+}
+
+check_machine_name() {
+    local machine=$1
+
+    for layer in ${LAYERS[@]}; do
+        machine_file=$(find $layer -name "${machine}.conf" -print)
+        if [ -n "$machine_file" ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+set_machine() {
+    local machine=$1
+
+    # Check if that machine is declared in the layers
+    if ! check_machine_name $machine; then
+        return 1
+    fi
+
+    if [ $QEMU = true ]; then
+        MACH_LOCAL_CONF="${machine}-qemu"
+    else
+        MACH_LOCAL_CONF=$machine
+    fi
+
+    echo "Yocto machine: ${MACH_LOCAL_CONF}"
+    sed -e 's:^\(MACHINE\).*:\1 = \"'${MACH_LOCAL_CONF}'\":' -i $BD/conf/local.conf
+}
+
+# Tune local.conf file
+if [ -n "${PROD}" ]; then
+    if [ -n "$ENABLE_RECOVERY" ]; then
+        if ! set_machine "${MACH}-${PROD}-rcy"; then
+            # If <MACH>-<PROD> is not available, fallback to <MACH>
+            set_machine "${MACH}"
+        fi
+    elif ! set_machine "${MACH}-${PROD}"; then
+        # If <MACH>-<PROD> is not available, fallback to <MACH>
+        set_machine "${MACH}"
+    fi
+else
+    set_machine "${MACH}"
+fi
+
+grep -E "SOURCE_MIRROR_URL" $BD/conf/local.conf > /dev/null
+if [ $? != 0 ]; then
+        sed -e '/^#DL_DIR/a\SOURCE_MIRROR_URL ?= \"file\:\/\/'"$scriptdir"'/../downloads\"\nINHERIT += \"own-mirrors\"\nBB_GENERATE_MIRROR_TARBALLS = \"1\"\nBB_NO_NETWORK = \"0\"\nWORKSPACE = \"'"${WORKSPACE}"'\"\nLINUX_REPO_DIR = \"'"${LINUXDIR}"'\"\nDISTRO = \"'"${DISTRO}"'\"' -i $BD/conf/local.conf
+fi
+sed -e 's:^#\(BB_NUMBER_THREADS\).*:\1 = \"'"$TASKS"'\":' -i $BD/conf/local.conf
+sed -e 's:^#\(PARALLEL_MAKE\).*:\1 = \"-j '"$THREADS"'\":' -i $BD/conf/local.conf
+
+set_option "LEGATO_BUILD" $ENABLE_LEGATO
+
+if [ -n "$FW_VERSION" ]; then
+    FW_VERSION_ENTRY=$(grep "FW_VERSION =" $BD/conf/local.conf)
+    if [ $? != 0 ]; then
+        # Append
+        sed -e '/^WORKSPACE/a\FW_VERSION = \"'"${FW_VERSION}"'\"\n' -i $BD/conf/local.conf
+    else
+        CURRENT_FW_VERSION=$(echo $FW_VERSION_ENTRY | sed -e 's/FW_VERSION = \"\(.*\)\"/\1/g')
+        if [[ "$FW_VERSION" != "$CURRENT_FW_VERSION" ]]; then
+            # Update entry
+            echo "Updating FW_VERSION from '${CURRENT_FW_VERSION}' to '${FW_VERSION}'"
+            sed -e 's/FW_VERSION = \".*\"/FW_VERSION = \"'"${FW_VERSION}"'\"/g' -i $BD/conf/local.conf
+        else
+            echo "Keeping FW_VERSION '${FW_VERSION}'"
+        fi
+    fi
+fi
+
+set_option "ROOTFS_VERSION" $ROOTFS_VERSION
+
+# Add or update extra options
+if [ -n "$X_OPTS" ]
+then
+    EXTRA_OPTS=$(echo $X_OPTS | sed -e "s/\:\:/ /g")
+    for OPT in $EXTRA_OPTS
+    do
+        opt_name=$(echo $OPT | cut -d"=" -f1)
+        opt_val=$(echo $OPT | cut -d"=" -f2-)
+
+        set_option $opt_name $opt_val
+    done
+fi
+
+# Kernel provider
+if [ -z "$KERNEL_PROVIDER" ]; then
+    case $MACH in
+        swi-mdm9x15 )
+            KERNEL_PROVIDER="linux-yocto"
+            ;;
+        swi-mdm9* )
+            KERNEL_PROVIDER="linux-quic"
+            ;;
+        * )
+            KERNEL_PROVIDER="linux-yocto"
+            ;;
+    esac
+fi
+
+grep -E "PREFERRED_PROVIDER_virtual\/kernel" $BD/conf/local.conf > /dev/null
+if [ $? != 0 ]; then
+    echo "PREFERRED_PROVIDER_virtual/kernel = \"${KERNEL_PROVIDER}\"" >> $BD/conf/local.conf
+else
+    sed -e 's:^\(PREFERRED_PROVIDER_virtual\/kernel\).*:\1 = \"'${KERNEL_PROVIDER}'\":' -i $BD/conf/local.conf
+fi
+
+# IceCC
+if [ $ENABLE_ICECC = true ]; then
+    if ! grep icecc $BD/conf/local.conf > /dev/null; then
+        echo 'INHERIT += "icecc"' >> $BD/conf/local.conf
+        echo 'ICECC_PARALLEL_MAKE = "-j 20"' >> $BD/conf/local.conf
+        echo 'ICECC_USER_PACKAGE_BL = "ncurses e2fsprogs libx11 gmp libcap perl busybox lk libgpg-error libarchive"' >> $BD/conf/local.conf
+    fi
+fi
+
+# Initramfs
+case $MACH in
+    swi-mdm* )
+        set_option 'INITRAMFS_IMAGE_BUNDLE' '1'
+        if test x$ENABLE_RECOVERY = "xtrue"; then
+            set_option 'INITRAMFS_IMAGE' "mdm-image-recovery"
+        else
+            set_option 'INITRAMFS_IMAGE' "${MACH#swi-}-image-initramfs"
+        fi
+        ;;
+esac
+
+# Firmware Path
+set_option 'FIRMWARE_PATH' "${FIRMWARE_PATH}"
+
+cd $BD
+
+# Command line
+if [ $CMD_LINE = true ]; then
+    /bin/bash
+    exit $?
+fi
+
+# Toolchain
+if [ $TOOLCHAIN = true ]; then
+    case $MACH in
+       swi-mdm* )
+           bitbake meta-toolchain-swi-ext
+           ;;
+       * )
+           bitbake meta-toolchain-swi
+           ;;
+    esac
+    exit $?
+fi
+
+# Images
+echo -n "Build image of "
+if [ $DEBUG = true ]; then
+    echo "dev rootfs (for $MACH)."
+    sed -e 's:^\(PACKAGE_CLASSES\).*:\1 = \"package_rpm\":' -i $BD/conf/local.conf
+    case $MACH in
+        swi-mdm* )
+            bitbake ${MACH#swi-}-image-dev
+            ;;
+        swi-virt* )
+            bitbake swi-virt-image-dev
+            ;;
+        * )
+            bitbake core-image-dev
+            ;;
+    esac
+    exit $?
+else
+    echo "minimal rootfs (for $MACH)."
+    sed -e 's:^\(PACKAGE_CLASSES\).*:\1 = \"package_ipk\":' -i $BD/conf/local.conf
+    case $MACH in
+        swi-mdm* )
+            if test x$ENABLE_RECOVERY = "xtrue"; then
+                bitbake mdm-image-recovery
+            else
+                bitbake ${MACH#swi-}-image-minimal
+            fi
+            ;;
+        swi-virt* )
+            bitbake swi-virt-image-minimal
+            ;;
+        * )
+            bitbake core-image-minimal
+            ;;
+    esac
+    exit $?
+fi
+
---
+++ repo.mk
@@ -0,0 +1,330 @@
+# Set number of threads
+NUM_THREADS ?= 9
+
+# Set workspace directory
+APPS_DIR ?= $(firstword $(wildcard $(PWD)/mdm*[0-9]/apps_proc))
+
+# Machine architecture
+MACH ?= $(patsubst $(PWD)/%/apps_proc,%,$(APPS_DIR))
+ifeq ($(MACH),)
+  ifneq (,$(wildcard $(PWD)/meta-swi-extras/meta-swi-mdm9x15-bin/files))
+    MACH := mdm9x15
+  else ifneq (,$(wildcard $(PWD)/meta-swi-extras/meta-swi-mdm9x28-ar758x-bin/files))
+    MACH := mdm9x28
+    PROD ?= ar758x
+  else ifneq (,$(wildcard $(PWD)/meta-swi-extras/meta-swi-mdm9x28-bin/files))
+    MACH := mdm9x28
+  else ifneq (,$(wildcard $(PWD)/meta-swi-extras/meta-swi-mdm9x50-bin/files))
+    MACH := mdm9x50
+  endif
+endif
+
+ifneq ($(MACH),)
+  MACH_ARGS := -m swi-$(MACH)
+endif
+
+# Try to get product name from manifest.xml
+ifeq ($(PROD),)
+  PROD := $(shell sed -nr '/product name/{s/.*name="(.*)".*/\1/;p}' $(shell pwd)/.repo/manifest.xml)
+endif
+
+# Try to get a default product name based on the manifest name if failed to get it from manifest
+ifeq ($(PROD),)
+  PROD := $(basename $(notdir $(shell readlink -f $(shell pwd)/.repo/manifest.xml)))
+endif
+
+ifneq ($(PROD),)
+  PROD_ARGS := -P $(PROD)
+endif
+
+# This assumes you have used repo and you have legato synced
+LEGATO_WORKDIR ?= $(shell pwd)/legato/
+
+# Build with legato ?
+ifndef LEGATO_BUILD
+  ifneq (,$(wildcard $(LEGATO_WORKDIR)))
+    LEGATO_BUILD := 1
+  else
+    $(warning "Legato: build disabled since '${LEGATO_WORKDIR}' does not exist")
+    LEGATO_BUILD := 0
+  endif
+endif
+
+# Build for mangOH ?
+MANGOH_BUILD ?= $(shell [ -d meta-mangoh ] && echo -n 1)
+
+# Include proprietary meta-swi-extras layer by default if existing
+PROPRIETARY_BUILD ?= $(shell [ -d meta-swi-extras ] && echo -n 1 || echo -n 0)
+
+# Use docker abstraction ?
+USE_DOCKER ?= 0
+
+# Use distributed building ?
+USE_ICECC ?= 0
+
+# Firmware path pointing to ar_yocto-cwe.tar.bz2
+FIRMWARE_PATH ?= 0
+
+# Toolchain prefix
+SDK_PREFIX ?= 0
+
+all: image_bin
+
+clean:
+	rm -rf build_*
+
+ifeq ($(USE_ICECC),1)
+  ICECC_ARGS = -h
+endif
+
+ifdef FW_VERSION
+  FW_VERSION_ARG := -v $(FW_VERSION)
+endif
+
+ifeq ($(LEGATO_BUILD),1)
+  ifdef LEGATO_WORKDIR
+    LEGATO_ARGS := -g -a "LEGATO_WORKDIR=${LEGATO_WORKDIR}"
+  endif
+endif
+
+ifeq ($(MANGOH_BUILD),1)
+  MANGOH_WIFI_REPO := "$(PWD)/mangOH/WiFi"
+  MANGOH_ARGS := -M \
+                 -a "MANGOH_WIFI_REPO=${MANGOH_WIFI_REPO}"
+endif
+
+ifneq ($(FIRMWARE_PATH),0)
+  FIRMWARE_PATH_ARGS := -F $(FIRMWARE_PATH)
+endif
+
+ifneq ($(SDK_PREFIX),0)
+  SDK_PREFIX_ARGS := -a "SDKPATH_PREFIX=${SDK_PREFIX}"
+endif
+
+ifdef TARGET_HOSTNAME
+  HOSTNAME_ARGS := -a "hostname_pn-base-files=${TARGET_HOSTNAME}"
+endif
+
+# Determine path for LK repository
+# On mdm9x15, lk is built using CAF revision + patches
+ifneq (,$(wildcard $(PWD)/lk/))
+  LK_REPO ?= "$(PWD)/lk"
+else ifneq (,$(wildcard $(APPS_DIR)/bootable/bootloader/lk/))
+  LK_REPO ?= "$(APPS_DIR)/bootable/bootloader/lk/"
+endif
+ifneq (,$(LK_REPO))
+  ifneq (mdm9x15,$(MACH))
+    LK_ARGS := -a "LK_REPO=$(LK_REPO)"
+  endif
+endif
+
+ifeq ($(RECOVERY_BUILD),1)
+  RCY_ARGS = -e
+endif
+
+# Replaces this Makefile by a symlink to repo.mk
+$(shell if ! test -h Makefile; then rm -f Makefile && ln -s meta-swi/repo.mk Makefile; fi)
+
+BUILD_SCRIPT := "meta-swi/build.sh"
+
+# Provide a docker abstraction for Yocto building, allowing the host seen
+# by the Yocto environment to be the ideal Linux distribution
+ifeq ($(USE_DOCKER),1)
+  UID := $(shell id -u)
+  HOSTNAME := $(shell hostname)
+  DOCKER_BIN ?= docker
+  DOCKER_IMG ?= "corfr/yocto-dev"
+  DOCKER_RUN := ${DOCKER_BIN} run \
+                    --rm \
+                    --user=${UID} \
+                    --tty --interactive \
+                    --hostname=${HOSTNAME} \
+                    --volume ${PWD}:${PWD} \
+                    --volume /etc/passwd:/etc/passwd \
+                    --volume /etc/group:/etc/group \
+                    --workdir ${PWD}
+  BUILD_SCRIPT := ${DOCKER_RUN} ${DOCKER_IMG} ${BUILD_SCRIPT}
+endif
+
+COMMON_ARGS := ${BUILD_SCRIPT} \
+				-p poky/ \
+				-o meta-openembedded/ \
+				-l meta-swi \
+				-x "kernel/.git" \
+				-j $(NUM_THREADS) \
+				-t $(NUM_THREADS) \
+				${ICECC_ARGS} \
+				${LEGATO_ARGS} \
+				${FIRMWARE_PATH_ARGS} \
+				${SDK_PREFIX_ARGS} \
+				${HOSTNAME_ARGS}
+
+# Machine:
+
+include $(wildcard $(MACH)/fw.mk)
+
+# Machine: swi-mdm9x15
+
+ifeq (mdm9x15,$(MACH))
+
+  # Create branches KBRANCH / KMETA in kernel
+
+  KBRANCH_mdm9x15_REV := $(shell cd kernel && git rev-parse HEAD | cut -c 1-10 -)
+  KMETA_mdm9x15_REV := $(shell cd kernel-meta && git rev-parse HEAD | cut -c 1-10 -)
+
+  ifndef KBRANCH_mdm9x15
+    KBRANCH_mdm9x15 := $(shell git --git-dir=kernel/.git rev-parse --symbolic-full-name m/master | grep -v 'm/master' | grep -v tags | sed 's^refs/remotes/[-_a-z0-9]*/^^gi')
+    ifeq ($(KBRANCH_mdm9x15),)
+      KBRANCH_mdm9x15 := $(shell git --git-dir=kernel/.git branch -r --contains $(KBRANCH_mdm9x15_REV) | grep -v 'm/master' | sed 's!^ *[-_a-z0-9]*/!!gi' | head -1)
+      ifeq ($(KBRANCH_mdm9x15),)
+        KBRANCH_mdm9x15 := "standard/swi-mdm9x15-unknown"
+      endif
+    endif
+  endif
+
+  ifndef KMETA_mdm9x15
+    KMETA_mdm9x15 := $(shell git --git-dir=kernel-meta/.git rev-parse --symbolic-full-name m/master | grep -v 'm/master' | grep -v tags | sed 's^refs/remotes/[-_a-z0-9]*/^^gi')
+    ifeq ($(KMETA_mdm9x15),)
+      KMETA_mdm9x15 := $(shell git --git-dir=kernel-meta/.git branch -r --contains $(KMETA_mdm9x15_REV) | grep -v 'm/master' | sed 's!^ *[-_a-z0-9]*/!!gi' | head -1)
+      ifeq ($(KMETA_mdm9x15),)
+        KMETA_mdm9x15 := "meta-yocto-unknown"
+      endif
+    endif
+  endif
+
+  KBRANCH_mdm9x15_CURRENT_REV := $(shell cd kernel && git show-ref -s refs/heads/${KBRANCH_mdm9x15} | cut -c 1-10 -)
+  KMETA_mdm9x15_CURRENT_REV := $(shell cd kernel && git show-ref -s refs/heads/${KMETA_mdm9x15} | cut -c 1-10 -)
+
+  KBRANCH_mdm9x15_BRIEF := $(shell git --git-dir=kernel/.git log -1 --pretty=oneline | sed "s/'//g")
+  KMETA_mdm9x15_BRIEF := $(shell git --git-dir=kernel-meta/.git log -1 --pretty=oneline | sed "s/'//g")
+
+  PREPARE_TASKS += kernel_branches
+
+.PHONY: kernel_branches
+kernel_branches:
+	@echo 'kernel KBRANCH_mdm9x15 (${KBRANCH_mdm9x15}): ${KBRANCH_mdm9x15_BRIEF}'
+	@if ! ( cd kernel && git branch |grep ${KBRANCH_mdm9x15} > /dev/null ); then \
+		echo "Create dev kernel branch ${KBRANCH_mdm9x15}" ; \
+		cd kernel && git branch ${KBRANCH_mdm9x15} ${KBRANCH_mdm9x15_REV} ; \
+	fi
+	@if [ "${KBRANCH_mdm9x15_CURRENT_REV}" != "${KBRANCH_mdm9x15_REV}" ]; then \
+		echo "=> Updating dev kernel branch ${KBRANCH_mdm9x15}: ${KBRANCH_mdm9x15_REV}" ; \
+		cd kernel && git branch -f ${KBRANCH_mdm9x15} ${KBRANCH_mdm9x15_REV} ; \
+	fi
+	@echo "kernel KMETA_mdm9x15 (${KMETA_mdm9x15}): ${KMETA_mdm9x15_BRIEF}"
+	@if ! ( cd kernel && git branch |grep ${KMETA_mdm9x15} > /dev/null ); then \
+		echo "Create dev kernel meta branch ${KMETA_mdm9x15}: ${KMETA_mdm9x15_REV}" ; \
+		cd kernel && git branch ${KMETA_mdm9x15} ${KMETA_mdm9x15_REV} ; \
+	fi
+	@if [ "${KMETA_mdm9x15_CURRENT_REV}" != "${KMETA_mdm9x15_REV}" ]; then \
+		echo "=> Updating dev kernel meta branch ${KMETA_mdm9x15}: ${KMETA_mdm9x15_REV}" ; \
+		cd kernel && git branch -f ${KMETA_mdm9x15} ${KMETA_mdm9x15_REV} ; \
+	fi
+	@if ! ( cd kernel && git branch | grep master > /dev/null ); then \
+		echo "Create local master branch" ; \
+		cd kernel && git branch master gerrit/master ; \
+	fi
+
+endif
+
+.PHONY: prepare
+prepare: $(PREPARE_TASKS)
+
+COMMON_MACH := \
+				$(COMMON_ARGS) \
+				$(MANGOH_ARGS) \
+				$(LK_ARGS) \
+				$(MACH_ARGS) \
+				${PROD_ARGS} \
+				$(RCY_ARGS)
+
+COMMON_BIN := \
+				$(COMMON_MACH) \
+				-b build_bin
+
+ifeq ($(PROPRIETARY_BUILD),1)
+  COMMON_BIN += -q
+endif
+
+QEMU_ARGS ?=
+ifeq ($(QEMU),1)
+  QEMU_ARGS := -Q
+endif
+
+COMMON_SRC := \
+				$(COMMON_MACH) \
+				${QEMU_ARGS} \
+				-b build_src
+
+ifeq ($(PROPRIETARY_BUILD),1)
+  COMMON_SRC += -w $(APPS_DIR) $(FW_VERSION_ARG) -s
+endif
+
+## images
+
+image_bin: prepare
+	$(COMMON_BIN)
+
+image_src: prepare
+	$(COMMON_SRC)
+
+## toolchains
+
+toolchain_bin: prepare
+	$(COMMON_BIN) -k
+
+toolchain_src: prepare
+	$(COMMON_SRC) -k
+
+## dev shell
+
+dev_bin: prepare
+	$(COMMON_BIN) -c
+
+dev_src: prepare
+	$(COMMON_SRC) -c
+
+dev: dev_bin
+
+# Machine: swi-virt
+
+COMMON_VIRT_ARM := \
+				$(COMMON_ARGS) \
+				-m swi-virt-arm \
+				-b build_virt-arm
+
+COMMON_VIRT_X86 := \
+				$(COMMON_ARGS) \
+				-m swi-virt-x86 \
+				-b build_virt-x86
+
+## images
+
+image_virt_arm:
+	$(COMMON_VIRT_ARM)
+
+image_virt_x86:
+	$(COMMON_VIRT_X86)
+
+image_virt: image_virt_arm
+
+## toolchains
+
+toolchain_virt_arm:
+	$(COMMON_VIRT_ARM) -k
+
+toolchain_virt_x86:
+	$(COMMON_VIRT_X86) -k
+
+toolchain_virt: toolchain_virt_arm
+
+## dev shell
+
+dev_virt_arm:
+	$(COMMON_VIRT_ARM) -c
+
+dev_virt_x86:
+	$(COMMON_VIRT_X86) -c
+
+dev_virt: dev_virt_arm
+
